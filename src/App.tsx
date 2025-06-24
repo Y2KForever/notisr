f1fd65a9064114542fdd229a18e1274b3d2ef9b8
Y2KForever <niklas.hansen1@gmail.com>
2025-06-25 00:26:27 +0200

    feat: Barebone setup for frontend

--- src/App.tsx
+++ src/App.tsx
@@ -1,51 +1,30 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import { invoke } from "@tauri-apps/api/core";
-import "./App.css";
+import { useEffect } from 'react';
+import './App.css';
+import { Glitch } from './components/Glitch';
+import { Button } from './components/ui/button';
+import { invoke } from '@tauri-apps/api/core';
+import { listen } from '@tauri-apps/api/event';
 
-function App() {
-  const [greetMsg, setGreetMsg] = useState("");
-  const [name, setName] = useState("");
-
-  async function greet() {
-    // Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
-    setGreetMsg(await invoke("greet", { name }));
-  }
+export const App = () => {
+  useEffect(() => {
+    listen("logged_in", (event) => {
+      invoke('shutdown_server');
+    })
+  }, []);
 
   return (
-    <main className="container">
-      <h1>Welcome to Tauri + React</h1>
-
-      <div className="row">
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo vite" alt="Vite logo" />
-        </a>
-        <a href="https://tauri.app" target="_blank">
-          <img src="/tauri.svg" className="logo tauri" alt="Tauri logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className="flex flex-col h-screen justify-center align-center">
+      <div className="flex justify-center">
+        <Button
+          onClick={() => {
+            invoke('login');
+          }}
+          className={`bg-[#9146FF] text-center align-center cursor-pointer`}
+        >
+          <Glitch />
+          Login with Twitch
+        </Button>
       </div>
-      <p>Click on the Tauri, Vite, and React logos to learn more.</p>
-
-      <form
-        className="row"
-        onSubmit={(e) => {
-          e.preventDefault();
-          greet();
-        }}
-      >
-        <input
-          id="greet-input"
-          onChange={(e) => setName(e.currentTarget.value)}
-          placeholder="Enter a name..."
-        />
-        <button type="submit">Greet</button>
-      </form>
-      <p>{greetMsg}</p>
-    </main>
+    </div>
   );
-}
-
-export default App;
+};
