ec5cfe3f41710d2676e04cd81001007282aa538c
Y2KForever <niklas.hansen1@gmail.com>
2025-06-25 19:24:29 +0200

    feat: Startup routine

---
+++ src/views/LogIn.tsx
@@ -0,0 +1,21 @@
+import { Glitch } from '@/components/Glitch';
+import { Button } from '@/components/ui/button';
+import { invoke } from '@tauri-apps/api/core';
+
+export const LogIn = () => {
+  return (
+    <div className="flex flex-col h-screen justify-center align-center">
+      <div className="flex justify-center">
+        <Button
+          onClick={() => {
+            invoke('login');
+          }}
+          className={`bg-[#9146FF] text-center align-center cursor-pointer`}
+        >
+          <Glitch />
+          Login with Twitch
+        </Button>
+      </div>
+    </div>
+  );
+};
