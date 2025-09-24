64e4f189fa7906e7fe539471a935efa20acaa74a
Y2KForever <niklas.hansen1@gmail.com>
2025-09-24 18:34:09 +0200

    feat: Updater

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import './App.css';
 import { invoke } from '@tauri-apps/api/core';
 import { listen, UnlistenFn } from '@tauri-apps/api/event';
@@ -6,10 +6,45 @@ import { LogIn } from './views/LogIn';
 import { List } from './views/List';
 import { Menu } from './components/Menu';
 import { Separator } from '@/components/ui/separator';
+import { check } from '@tauri-apps/plugin-updater';
+import { ask, message } from '@tauri-apps/plugin-dialog';
+import { relaunch } from '@tauri-apps/plugin-process';
+
+export const checkForUpdate = async (onUserClick: false) => {
+  const update = await check();
+  if (update) {
+    const yes = await ask(`Update to ${update.version} is available! \n\n\ Release notes: ${update.body}`, {
+      title: 'Update available',
+      kind: 'info',
+      okLabel: 'Update',
+      cancelLabel: 'Cancel',
+    });
+    if (yes) {
+      await update.downloadAndInstall();
+      await relaunch();
+    }
+  } else if (onUserClick) {
+    await message('You are on the latest version', {
+      title: 'No Update Available',
+      kind: 'info',
+      okLabel: 'OK',
+    });
+  }
+};
 
 export const App = () => {
   const [layout, setLayout] = useState<string>('list');
   const [loading, setLoading] = useState<boolean>(true);
+  const hasCheckedUpdate = useRef(false); // Guard strictMode
+  const version = useRef<string | undefined>(undefined);
+
+  useEffect(() => {
+    check().then((val) => {
+      console.log(val);
+      version.current = val?.currentVersion;
+    });
+  }, []);
+
   useEffect(() => {
     invoke('on_startup').then((val) => {
       if (val === 'log_in') {
@@ -25,6 +60,13 @@ export const App = () => {
     }).then((fn) => {
       unlistenLoggedIn = fn;
     });
+
+    if (!hasCheckedUpdate.current) {
+      hasCheckedUpdate.current = true;
+      setTimeout(() => {
+        checkForUpdate(false).catch((e) => console.error('Update check error: ', e));
+      }, 600);
+    }
     return () => {
       unlistenLoggedIn && unlistenLoggedIn();
     };
@@ -33,6 +75,7 @@ export const App = () => {
   return (
     <div className="h-[100%] w-full dark:bg-[#26262c] bg-[#efeff1] flex flex-col">
       <Menu />
+      {version.current && import.meta.env.DEV && <p>{version.current}</p>}
       <Separator className="mt-1 mb-2" />
       {layout === 'login' ? <LogIn /> : layout === 'list' && <List setLoading={setLoading} loading={loading} />}
     </div>
