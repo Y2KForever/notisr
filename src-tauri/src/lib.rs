ffd894c672049510becbc44e32bdacccbe0d08dc
Y2KForever <niklas.hansen1@gmail.com>
2025-09-24 18:33:52 +0200

    feat: Updater

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -308,6 +308,9 @@ pub fn run() {
     .plugin(tauri_plugin_opener::init())
     .plugin(tauri_plugin_positioner::init())
     .plugin(tauri_plugin_notification::init())
+    .plugin(tauri_plugin_updater::Builder::new().build())
+    .plugin(tauri_plugin_dialog::init())
+    .plugin(tauri_plugin_process::init())
     .invoke_handler(tauri::generate_handler![
       shutdown_server,
       on_startup,
